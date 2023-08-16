768f791a42e296b392ce46952f185c1ebfff0419
YBMLH <yasserbmlh24@gmail.com>
2023-08-16 19:36:27 +0200

    img slideshow

--- Portfolio for tests/app.js
+++ Portfolio for tests/app.js
@@ -35,3 +35,26 @@ function typeWriter() {
 }
 
 typeWriter();
+
+
+
+// for the img slide
+// script.js
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let slides = document.getElementsByClassName("slide");
+  for (let i = 0; i < slides.length; i++) {
+    slides[i].style.opacity = 0; // Hide all images
+  }
+  
+  slideIndex++;
+  if (slideIndex > slides.length) {
+    slideIndex = 1;
+  }
+  
+  slides[slideIndex - 1].style.opacity = 1; // Show current image
+  setTimeout(showSlides, 6000); // Change slide every 3 seconds
+}
+
